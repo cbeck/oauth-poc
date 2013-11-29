0a0c18ff40e8f3f50314283ff740c3cb44dd50f6
Chris Beck <chris.beck@me.com>
2013-11-29 16:38:05 -0500

    monitor traffic

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ SETUP_PROC = lambda do |env|
   env['omniauth.strategy'].options[:client_id] = ENV['SURVEY_MONKEY_CLIENT_ID']
   env['omniauth.strategy'].options[:api_key] = ENV['SURVEY_MONKEY_API_KEY']
   env['omniauth.strategy'].options[:api_secret] = ENV['SURVEY_MONKEY_SECRET']
+  env['omniauth.strategy'].options[:client_options][:site] = 'https://api-surveymonkey-com-fytofsd4ktc2.runscope.net'
 end
    
 
