88bf709418c10fffc1fe2d10e61bc66be1f0a917
Chris Beck <chris.beck@me.com>
2013-11-29 10:23:40 -0500

    was passing client id but using api key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :surveymonkey, ENV['SURVEY_MONKEY_API_KEY']
+  provider :surveymonkey, , ENV['SURVEY_MONKEY_CLIENT_ID'], ENV['SURVEY_MONKEY_API_KEY']
 end
 
 OmniAuth.config.logger = Rails.logger
