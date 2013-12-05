9fb9c9aff26f816425e73f61d8afccd6c57eb2d4
Chris Beck <chris.beck@me.com>
2013-12-04 22:10:52 -0500

    maybe it doesn't like the data key?

--- app/controllers/api_accounts_controller.rb
+++ app/controllers/api_accounts_controller.rb
@@ -14,7 +14,7 @@ class ApiAccountsController < ApplicationController
     data["order_asc"] = true
     @response = HTTParty.post("https://api-surveymonkey-net-fytofsd4ktc2.runscope.net/v2/surveys/get_survey_list",
         :query => {:api_key => ENV['SURVEY_MONKEY_API_KEY']},
-        :body => {data: JSON.dump(data)},
+        :body => JSON.dump(data),
         :headers => { "Authorization" => "Bearer #{@api_account.oauth_token}", "Content-Type" => "application/json"}
       )  
   end
