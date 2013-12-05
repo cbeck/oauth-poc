aca8836cc598fcba53814f1297cac1beda27f191
Chris Beck <chris.beck@me.com>
2013-12-04 20:42:05 -0500

    damn misnamed controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ OauthPoc::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'api_accounts#index'
+  root 'api_account#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
